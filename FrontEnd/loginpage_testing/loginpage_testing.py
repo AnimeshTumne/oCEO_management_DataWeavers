c3cb8427469d4e04b2cea431091b9e03ff8b86d6
Animesh Tumne <animeshtumne@iitgn.ac.in>
2024-03-29 03:19:02 +0530

    Removed new user from individual pages, and added to main page.

--- FrontEnd/loginpage_testing/loginpage_testing.py
+++ FrontEnd/loginpage_testing/loginpage_testing.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for 
+from flask import *
+from werkzeug.security import generate_password_hash
+import os
+import flask_mysqldb as mysql
+from flask_mysqldb import MySQL
+# --------------------------------------------------------------------------------------------------------------------------------------
 
 app = Flask(__name__)
+
+# --------------------------------------------------------------------------------------------------------------------------------------
 
 # Home Page - Select User Type
 @app.route('/')
 def index():
     return render_template('loginpage_testing.html')
+
+# --------------------------------------------------------------------------------------------------------------------------------------
 
 # Login Pages
 @app.route('/login/student', methods=['GET', 'POST'])
@@ -32,26 +42,41 @@ def login_admin():
         return redirect(url_for('query_page'))
     return render_template('loginpage_admin.html')
 
-@app.route('/login/successful_test', methods=['GET', 'POST'])
-def login_successful():
-    return render_template('loginpage_successful.html')
+# --------------------------------------------------------------------------------------------------------------------------------------
 
 @app.route('/login/new_user', methods=['GET', 'POST'])
 def login_new_user():
     if request.method == 'POST':
         # Handle new user registration
-        # Redirect to the login page on successful registration
-        return redirect(url_for('index'))
+        # username = request.form['username']
+        # password = request.form['password']
+        
+        # # Execute the CREATE USER query using the form data
+        # query = f"CREATE USER {username} IDENTIFIED BY '{password}'"
+        # # Execute the query using your database library
+        # # For example, if you are using MySQL library:
+        # cur = mysql.connection.cursor()
+        # cur.execute(query)
+        # mysql.connection.commit()
+        # cur.close()
+        # Redirect to the query page on successful registration
+        return redirect(url_for('nu_aftersubmit'))
     return render_template('loginpage_newuser.html')
 
+# --------------------------------------------------------------------------------------------------------------------------------------
+
+@app.route('/login/nu_aftersubmit', methods=['GET', 'POST'])
+def nu_aftersubmit():
+    return render_template('loginpage_aftersubmit.html')
+
 # Query Page
-@app.route('/query', methods=['GET', 'POST'])
-def query_page():
-    if request.method == 'POST':
-        # Handle SELECT query execution
-        # Display result if successful, otherwise prompt user to input another query
-        return render_template('query_result.html', result=result)
-    return render_template('query.html')
+# @app.route('/query', methods=['GET', 'POST'])
+# def query_page():
+#     if request.method == 'POST':
+#         # Handle SELECT query execution
+#         # Display result if successful, otherwise prompt user to input another query
+#         return render_template('query_result.html', result=result)
+#     return render_template('query.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
