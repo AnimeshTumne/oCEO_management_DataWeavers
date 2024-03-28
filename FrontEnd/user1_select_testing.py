a217047ae199b96b5340a93459acc968be9c6d44
Animesh Tumne <animeshtumne@iitgn.ac.in>
2024-03-28 22:25:44 +0530

    Added initial testing files to see that database is properly connected to the front-end.

---
+++ FrontEnd/user1_select_testing.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+
+# -----------------------------------------------------------------------------------------------------------------------------------------
+
+app = Flask(__name__)
+
+# -----------------------------------------------------------------------------------------------------------------------------------------
+
+# MySQL Configuration - currently configured to user1
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'user1'
+app.config['MYSQL_PASSWORD'] = 'password1'
+app.config['MYSQL_DB'] = 'oceo_management'
+mysql = MySQL(app)
+
+# -----------------------------------------------------------------------------------------------------------------------------------------
+
+# define various landing pages below:
+
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM application_status")
+    data = cur.fetchall()
+    cur.close()
+    print(type(data))
+    return render_template('user1_select_testing.html', application_status=data)
+
+# -----------------------------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    app.run(debug=True)
