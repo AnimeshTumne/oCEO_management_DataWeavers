5fceb7ad0df98aa0cd0f109fc0fbb6ebc7388a19
Animesh Tumne <animeshtumne@iitgn.ac.in>
2024-04-04 02:00:56 +0530

    added pages for jobs_created in professor

--- main_project/main_app.py
+++ main_project/main_app.py
@@ -325,6 +325,7 @@ def student_personal_info_change():
         else:
             fetched_phone = ("Not Added", "Not Added")
 
+        cursor.close()
         return render_template('student/personal_info_change.html', student_data = fetched_data, student_phone = fetched_phone)
 
     else:
@@ -590,8 +591,7 @@ def after_login_professor():
                 case 'timecard_for_review':
                     return redirect(url_for('professor_timecard_for_review'))
                 case 'logout':
-                    session.pop("faculty_id", None)
-                    return redirect(url_for('index'))
+                    return redirect(url_for('professor_logout'))
                 case _:
                     return render_template('professor/after_login.html')
         cursor = db.connection.cursor()
@@ -607,10 +607,8 @@ def after_login_professor():
 def professor_personal_info():
     if "faculty_id" in session:
         if request.method == 'POST':
-            if request.form['submit_button'] == 'Update_profile':
-                return redirect(url_for('professor_edit_personal_info'))
-            elif request.form['submit_button'] == 'Add_job':
-                return redirect(url_for('professor_add_job'))
+            if request.form['submit_button'] == 'Update_Profile':
+                return redirect(url_for('professor_personal_info_change'))
             elif request.form['submit_button'] == 'Change_Password':
                 return redirect(url_for('professor_change_password'))
             
@@ -619,10 +617,226 @@ def professor_personal_info():
         cursor.execute(f"SELECT * FROM faculty WHERE faculty_id = {faculty_id};")
         fetched_data = cursor.fetchall()
         cursor.close()
-        return render_template('professor/personal_info.html', faculty_data=fetched_data)
+        return render_template('professor/personal_info.html', professor_data=fetched_data)
+    else:
+        return redirect(url_for('errorpage'))
+    
+@app.route('/professor/update_profile', methods=['GET', 'POST'])
+def professor_personal_info_change():
+    if "faculty_id" in session:
+        if request.method == 'POST':
+            faculty_id = session['faculty_id']
+            first_name = request.form.get('first_name')
+            middle_name = request.form.get('middle_name')
+            last_name = request.form.get('last_name')
+            dept_name = request.form.get('department_name')
+            voip_id = request.form.get('voip_id')
+            cursor = db.connection.cursor()
+            cursor.execute(f"UPDATE faculty SET first_name = '{first_name}', middle_name = '{middle_name}', last_name = '{last_name}', dept_name = '{dept_name}', voip_id = {voip_id} WHERE faculty_id = {faculty_id}")
+            db.connection.commit()
+
+            if 'image' in request.files:
+                image = request.files['image']
+                image_data = image.read()
+                cursor.execute(f"UPDATE faculty SET image = {image_data} WHERE faculty_id = {faculty_id};")
+                db.connection.commit()
+            
+            cursor.close()
+            return redirect(url_for('professor_personal_info'))
+        cursor = db.connection.cursor()
+        faculty_id = session['faculty_id']
+        cursor.execute(f"SELECT * FROM faculty WHERE faculty_id = {faculty_id};")
+        fetched_data = cursor.fetchall()
+        cursor.close()
+        return render_template('professor/personal_info_change.html', professor_data=fetched_data)
     else:
         return redirect(url_for('errorpage'))
 
+@app.route('/professor/change_password', methods=['GET', 'POST'])
+def professor_change_password():
+    if "faculty_id" in session:
+        if request.method == 'POST':
+            faculty_id = session['faculty_id']
+            cursor = db.connection.cursor()
+            old_password = request.form.get('old_password')
+            new_password = request.form.get('new_password')
+            confirm_password = request.form.get('confirm_password')
+            cursor.execute(f"SELECT password FROM faculty WHERE faculty_id = {faculty_id};")
+            hashed_password = cursor.fetchone()[0]
+            cursor.close()
+            if bcrypt.check_password_hash(hashed_password, old_password):
+                if new_password == confirm_password:
+                    new_hashed_password = bcrypt.generate_password_hash(new_password).decode("utf-8")
+                    cursor = db.connection.cursor()
+                    cursor.execute(f"UPDATE faculty SET password = '{new_hashed_password}' WHERE faculty_id = {faculty_id};")
+                    db.connection.commit()
+                    cursor.close()
+                    return redirect(url_for('professor_personal_info'))
+                else:
+                    return redirect(url_for('errorpage'))
+            else:
+                return redirect(url_for('errorpage'))
+        return render_template('professor/change_password.html')
+    else:
+        return redirect(url_for('errorpage'))
+
+@app.route('/professor/jobs_created', methods=['GET', 'POST'])
+def professor_jobs_created():
+    if "faculty_id" in session:
+        if request.method == 'POST':
+            if request.form['submit_button'] == 'change_job_details':
+                job_id = request.form['job_id']
+                return redirect(url_for('professor_jobs_change_details', job_id=job_id))
+            elif request.form['submit_button'] == 'add_job':
+                return redirect(url_for('professor_add_job'))
+            elif request.form['submit_button'] == 'delete_job':
+                job_id = request.form['job_id']
+                return redirect(url_for('professor_delete_job', job_id=job_id))
+            elif request.form['submit_button'] == 'view_applications':
+                job_id = request.form['job_id']
+                return redirect(url_for('professor_view_applications', job_id=job_id))
+        
+        cursor = db.connection.cursor()
+        faculty_id = session['faculty_id']
+        cursor.execute(f"SELECT job.* FROM job WHERE job.faculty_id = {faculty_id};")
+        job_data = cursor.fetchall()
+        cursor.execute("SHOW COLUMNS FROM job")
+        job_head = cursor.fetchall()
+        column_names = tuple(row[0] for row in job_head)
+        cursor.close()
+        return render_template('professor/jobs_created.html', job_data=job_data, job_head=column_names)
+    else:
+        return redirect(url_for('errorpage'))
+
+@app.route('/professor/add_job', methods=['GET', 'POST'])
+def professor_add_job():
+    if "faculty_id" in session:
+        if request.method == 'POST':
+            faculty_id = session['faculty_id']
+            job_type = request.form.get('job_type')
+            job_description = request.form.get('job_description')
+            min_qualifications = request.form.get('minimum_qualification')
+            job_criteria = request.form.get('job_criteria')
+            prerequisites = request.form.get('prerequisites')
+            additional_info = request.form.get('additional_information')
+            pay_per_hour = request.form.get('pay_per_hour')
+            no_of_positions = request.form.get('number_of_positions')
+            start_date = request.form.get('start_date')
+            end_date = request.form.get('end_date')
+            tenure = request.form.get('tenure')
+            application_deadline = request.form.get('application_deadline')
+            is_position_open = 1
+            cursor = db.connection.cursor()
+            cursor.execute("SELECT count(*) FROM job")
+            total_count = cursor.fetchone()[0]
+            if total_count == 0:
+                job_id = random.randint(12345678, 29999999)
+            else:
+                cursor.execute("SELECT MAX(job_id) FROM job")
+                max_id = cursor.fetchone()[0]
+                job_id = max_id + 1
+            if job_type == 'ADH':
+                job_id = 201
+                course_id = request.form.get('course_id')
+                course_name = request.form.get('course_name')
+                cursor.execute(f"INSERT INTO adh (job_id, course_id, course_name) VALUES ({job_id}, '{course_id}', '{course_name}');")
+            elif job_type == 'PAL':
+                subjects_to_teach = request.form.get('subjects_to_teach')
+                cursor.execute(f"INSERT INTO subjects_under_pal (job_id, subjects_to_teach) VALUES ({job_id}, '{subjects_to_teach}');")
+            else:
+                role_name = request.form.get('role_name')
+                cursor.execute(f"INSERT INTO others (job_id, role_name) VALUES ({job_id}, '{role_name}');")
+            cursor.execute(f"INSERT INTO job (job_id, job_type, job_description, min_qualifications, job_criteria, prerequisites, additional_info, pay_per_hour, no_of_positions, start_date, end_date, tenure, faculty_id, is_position_open, application_deadline) VALUES ({job_id}, '{job_type}', '{job_description}', '{min_qualifications}', '{job_criteria}', '{prerequisites}', '{additional_info}', {pay_per_hour}, {no_of_positions}, '{start_date}', '{end_date}', '{tenure}', {faculty_id}, {is_position_open}, '{application_deadline}');")
+            db.connection.commit()
+            cursor.close()
+            return redirect(url_for('professor_jobs_created'))
+        return render_template('professor/add_job.html')
+    else:
+        return redirect(url_for('errorpage'))
+
+@app.route('/professor/change_job_details/<job_id>', methods=['GET', 'POST'])
+def professor_jobs_change_details(job_id):
+    if "faculty_id" in session:
+        if request.method == 'POST':
+            faculty_id = session['faculty_id']
+            job_type = request.form.get('job_type')
+            job_description = request.form.get('job_description')
+            min_qualifications = request.form.get('minimum_qualification')
+            job_criteria = request.form.get('job_criteria')
+            prerequisites = request.form.get('prerequisites')
+            additional_info = request.form.get('additional_information')
+            pay_per_hour = request.form.get('pay_per_hour')
+            no_of_positions = request.form.get('number_of_positions')
+            start_date = request.form.get('start_date')
+            end_date = request.form.get('end_date')
+            tenure = request.form.get('tenure')
+            application_deadline = request.form.get('application_deadline')
+            cursor = db.connection.cursor()
+            if job_type == 'ADH':
+                course_id = request.form.get('course_id')
+                course_name = request.form.get('course_name')
+                cursor.execute(f"UPDATE adh SET course_id = '{course_id}', course_name = '{course_name}' WHERE job_id = {job_id};")
+            elif job_type == 'PAL':
+                subjects_to_teach = request.form.get('subjects_to_teach')
+                cursor.execute(f"UPDATE subjects_under_pal SET subjects_to_teach = '{subjects_to_teach}' WHERE job_id = {job_id};")
+            else:
+                role_name = request.form.get('role_name')
+                cursor.execute(f"UPDATE others SET role_name = '{role_name}' WHERE job_id = {job_id};")
+            cursor.execute(f"UPDATE job SET job_type = '{job_type}', job_description = '{job_description}', min_qualifications = '{min_qualifications}', job_criteria = '{job_criteria}', prerequisites = '{prerequisites}', additional_info = '{additional_info}', pay_per_hour = {pay_per_hour}, no_of_positions = {no_of_positions}, start_date = '{start_date}', end_date = '{end_date}', tenure = '{tenure}', faculty_id = {faculty_id}, application_deadline = '{application_deadline}' WHERE job_id = {job_id};")
+            db.connection.commit()
+            cursor.close()
+            return redirect(url_for('professor_jobs_created'))
+        
+        cursor = db.connection.cursor()
+        cursor.execute(f"SELECT job.* FROM job WHERE job.job_id = {job_id};")
+        job_data = cursor.fetchall()
+        cursor.execute("SHOW COLUMNS FROM job")
+        job_head = cursor.fetchall()
+        column_names = tuple(row[0] for row in job_head)
+        cursor.close()
+        return render_template('professor/change_job_details.html', job_data=job_data, job_head=column_names)
+    else:
+        return redirect(url_for('errorpage'))
+
+@app.route('/professor/delete_job/<job_id>', methods=['GET', 'POST'])
+def professor_delete_job(job_id):
+    if "faculty_id" in session:
+        cursor = db.connection.cursor()
+        
+        cursor.execute(f"select job_type from job where job_id = {job_id};")
+        job_type = cursor.fetchone()[0]
+        if job_type == "ADH":
+            cursor.execute(f"DELETE FROM adh WHERE job_id = {job_id};")
+        elif job_type == "PAL":
+            cursor.execute(f"DELETE FROM subjects_under_pal WHERE job_id = {job_id};")
+        # else:
+        #     cursor.execute(f"DELETE FROM others WHERE job_id = {job_id};")
+        
+        cursor.execute(f"DELETE FROM job WHERE job_id = {job_id};")
+        db.connection.commit()
+        cursor.close()
+        return redirect(url_for('professor_jobs_created'))
+    else:
+        return redirect(url_for('errorpage'))
+
+@app.route('/professor/view_applications/<job_id>', methods=['GET', 'POST'])
+def professor_view_applications(job_id):
+    if "faculty_id" in session:
+        cursor = db.connection.cursor()
+        cursor.execute(f"SELECT application_status.application_id, application_status.roll_number, applied_student.first_name, applied_student.middle_name, applied_student.last_name, applied_student.cpi, applied_student.last_sem_spi, applied_student.on_probation, application_status.approval, application_status.statement_of_motivation FROM application_status JOIN applied_student ON application_status.roll_number = applied_student.roll_number WHERE application_status.job_id = {job_id};")
+        application_data = cursor.fetchall()
+        cursor.execute("SHOW COLUMNS FROM application_status")
+        application_head = cursor.fetchall()
+        column_names = tuple(row[0] for row in application_head)
+        cursor.close()
+        return render_template('professor/view_applications.html', application_data=application_data, application_head=column_names)
+    else:
+        return redirect(url_for('errorpage'))
+
+@app.route('/professor/logout')
+def professor_logout():
+    session.pop('faculty_id', None)
+    return redirect(url_for('index'))
 # ------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     app.run(debug=True)
