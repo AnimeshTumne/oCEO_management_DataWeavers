fb5a3f79b3e772ea1d6283f94cc37c5c514694f6
Animesh Tumne <animeshtumne@iitgn.ac.in>
2024-04-03 16:37:32 +0530

    added queries to change the database based on final design

--- main_project/insert_passwords.py
+++ main_project/insert_passwords.py
@@ -43,11 +43,10 @@ def index():
         passwords.append("'" + hashed_password + "'")
     
     cur = db.connection.cursor()
-
-    # added query to add password column in applied_student table
-    query_add_column_password = "ALTER TABLE applied_student ADD COLUMN IF NOT EXISTS password VARCHAR(255);"
-    cur.execute(query_add_column_password)
     
+    # added query to add password column in applied_student table
+    query_add_column_password = "ALTER TABLE applied_student ADD COLUMN password VARCHAR(255);"
+    cur.execute(query_add_column_password)
     # rename column Approved to approval
     query_rename_approved_column = "ALTER TABLE application_status RENAME COLUMN Approved TO approval;"
     cur.execute(query_rename_approved_column)
