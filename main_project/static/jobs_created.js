8a24e1bc76f62f7e1a8cc7fcd8d68b588b557055
Animesh Tumne <animeshtumne@iitgn.ac.in>
2024-04-15 00:07:33 +0530

    Completed frontend for student. For others and professor is remaining.

--- main_project/static/jobs_created.js
+++ main_project/static/jobs_created.js
@@ -2,15 +2,18 @@ function toggleMenu() {
     var sidebar = document.getElementById('sidebar');
     var mainContent = document.getElementById('main-content');
     var menuIcon = document.querySelector('.menu-icon');
+	var mainContentAbove = document.getElementById('main-content-above');
     sidebar.classList.toggle('open');
     menuIcon.classList.toggle('open');
     mainContent.classList.toggle('main-content-shift');
+	mainContentAbove.classList.toggle('main-content-shift');
     // add transition effect - when clicked on anywhere in the document, sidebar should close
     document.addEventListener('click', function(event) {
         if (!event.target.closest('#sidebar') && !event.target.closest('.menu-icon')) {
             sidebar.classList.remove('open');
             menuIcon.classList.remove('open');
             mainContent.classList.remove('main-content-shift');
+			mainContentAbove.classList.remove('main-content-shift');
         }
     });
 }
